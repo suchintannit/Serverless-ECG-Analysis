cb8a9c8e9309a18af3e57c63f1e10cc1104e0949
suchintannit <suchintan.mishra@gmail.com>
2019-01-01 20:04:40 +0530

    Update index.js

--- src/getwelcome/index.js
+++ src/getwelcome/index.js
@@ -13,5 +13,16 @@ exports.handler = async event => {
     body: responseBody
   };
 
-  return response;
+ var params = {
+  FunctionName: 'Process', /* required */
+  ClientContext: 'STRING_VALUE',
+  InvocationType: Event | RequestResponse | DryRun,
+  LogType: None | Tail,
+  Payload: new Buffer('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
+  Qualifier: 'STRING_VALUE'
+};
+  lambda.invoke(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 };
